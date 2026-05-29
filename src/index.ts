eefc292cbee6e209df46126e8804ea1abbb07f6a
1996Vasukannan <vasukannan2996@gmail.com>
2026-05-29 18:29:35 +0530

    trigger pipeline

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-})
+});
 
 const connectWithRetry = async () => {
   let retries = 5;
